24febc24dc7c0136c0016bdd160ca15a2f305b86
henri <gontijohenrique87@gmail.com>
2026-06-19 09:06:36 -0300

    refazendo

--- script.js
+++ script.js
@@ -1,55 +1,19 @@
-
-
-class ContaBancaria{
-    #saldo;
+class Parquimetro{
+    
     constructor(){
-    this.#saldo = 0;
-    }
+        this.saldo = 0;
+        this.planoEscolhido = null;
+        this.intervalo = null;
+        this.tempoRestante = null;
 
-    depositar(valor){
-        this.#saldo += valor;
-    }
+        this.planos = {
+            pequeno:  { valor: 1.00,  minutos: 30  },
+            medio: { valor: 1.75,  minutos: 60  },
+            grande:   { valor: 3.00,  minutos: 120 }
+        };
 
-    sacar(valor){
-        this.#saldo -= valor;
-    }
+        this.iniciarEventos();
+    };
 
-    temSaldoParaSacar(valor){
-        return valor <= this.#saldo;
-    }
 
-    get saldo(){
-        return this.#saldo;
-    }
-}
-
-class CaixaEletronico{
-    constructor(conta){
-        this.conta = conta;
-    }
-
-    depositar(){
-        const valorDeposito = parseFloat(document.getElementById("valorDeposito").value);
-        this.conta.depositar(valorDeposito);
-        this.mostrarSaldo();
-    }
-
-    sacar(){
-        const valorSaque = parseFloat(document.getElementById("valorSaque").value);
-        if(this.conta.temSaldoParaSacar(valorSaque)){
-            this.conta.sacar(valorSaque);
-            this.mostrarSaldo(this.conta.saldo);
-        } else {
-            this.mostrarSaldo("Insuficiente!")
-        }
-    }
-
-    mostrarSaldo(){
-        document.getElementById("saldo").textContent = `Saldo R$ ${this.conta.saldo},00.`;
-        document.getElementById("valorDeposito").value = ``
-        document.getElementById("valorSaque").value = ``
-    }
-}
-
-const conta = new ContaBancaria();
-const caixaEletronico = new CaixaEletronico(conta);
+};
